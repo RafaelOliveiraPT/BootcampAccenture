00cf53421f2c04c1b4201182dd5fea0461b6da70
RafaelOliveiraPT <rafaelcreative@gmail.com>
2025-07-15 10:19:15 +0100

    new struct for folders

---
+++ JAVA/StockManager/src/pt/upacademy/stockmanager/Shelf.java
@@ -0,0 +1,63 @@
+package pt.upacademy.stockmanager;
+
+public class Shelf {
+	private int ID;
+	private static int globalID = 0;
+	private Product product;
+	private int capacity = 1;
+	private double diariaAluguer;
+
+	public Shelf() {
+		this.ID = globalID++;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		this.ID = iD;
+	}
+
+	public Product getProduto() {
+		return product;
+	}
+
+	public void setProduto(Product produto) {
+		this.product = produto;
+	}
+
+	public double getDiariaAluguer() {
+		return diariaAluguer;
+	}
+
+	public void setDiariaAluguer(double diariaAluguer) {
+		this.diariaAluguer = diariaAluguer;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	
+	public void clearShelf() {
+		this.capacity = 1;
+		this.product = null;
+	}
+
+	public void showShelf() {
+		StringBuilder auxStr = new StringBuilder();
+		auxStr.append("Prateleira:\n");
+		auxStr.append("ID=" + this.ID + "\n");
+		auxStr.append("Capacity=" + this.capacity + "\n");
+		if(this.product != null) {
+			auxStr.append("Produto:\n");
+			auxStr.append(product.getName()+ "\n");
+		}
+		auxStr.append("----------/----------");
+		System.out.println(auxStr);
+	}
+}
